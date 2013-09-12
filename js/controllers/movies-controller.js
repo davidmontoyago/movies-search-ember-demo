6452a7060093381b8079068d63f1d80d9d76c6c6
David Montoya <davidmontoyago@gmail.com>
2013-09-11 20:41:10 -0400

    Added director info.

--- js/controllers/movies-controller.js
+++ js/controllers/movies-controller.js
@@ -12,3 +12,14 @@ Movies.MoviesController = Ember.ObjectController.extend({
 		}
 	}
 });
+
+Movies.MovieController = Ember.ObjectController.extend({
+	directorInflection: function() {
+		var model = this.get('model');
+		var director = model.get('directors');
+		if (director && director != null) {
+			return director.indexOf(',') > 0 ? 'Directors' : 'Director';
+		}
+		return '';
+	}.property('model.directorInflection')
+});
